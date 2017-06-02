97340c0676fa0be81d2da7c8ac4f980926c112cf
Alvaro Ramirez <alvaro.ramirez@grupovaughan.com>
2017-06-02 16:39:02 +0200

    Improve error blades.

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -11,24 +11,9 @@
     <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
     <meta http-equiv="pragma" content="no-cache" />
     <title>500 | Test Center</title>
-    <!-- Favicon-->
-    <link rel="icon" href="{{ asset('/assets/img/favicon.ico')}}" type="image/x-icon">
 
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+    @include('layout._header')
 
-    <!-- Bootstrap Core Css -->
-    <link href="{{ asset('/assets/css/bootstrap/bootstrap.css')}}" rel="stylesheet">
-
-    <!-- Waves Effect Css -->
-    <link href="{{ asset('/assets/css/node-waves/waves.css')}}" rel="stylesheet">
-
-    <!-- Animation Css -->
-    <link href="{{ asset('/assets/css/animate-css/animate.css')}}" rel="stylesheet">
-
-    <!-- Custom Css -->
-    <link href="{{ asset('/assets/css/style.css')}}" rel="stylesheet">
 </head>
 
 <body class="four-zero-four">
@@ -36,19 +21,10 @@
         <div class="error-message">Gestor de TESTS</div>
         <div class="error-code">500 ERROR</div>
         <div class="error-message">Internal Server Error</div>
-        <div class="button-place">
-            <a href="/" class="btn btn-default btn-lg waves-effect">GO TO HOMEPAGE</a>
-        </div>
     </div>
 
-    <!-- Jquery Core Js -->
-    <script src="{{ asset('/assets/js/jquery/jquery.min.js')}}"></script>
+    @include('layout._footer')
 
-    <!-- Bootstrap Core Js -->
-    <script src="{{ asset('/assets/js/bootstrap/bootstrap.js')}}"></script>
-
-    <!-- Waves Effect Plugin Js -->
-    <script src="{{ asset('/assets/js/node-waves/waves.js')}}"></script>
 </body>
 
 </html>
